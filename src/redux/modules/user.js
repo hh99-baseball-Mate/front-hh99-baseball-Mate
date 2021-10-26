6af4c2280798100236c9f3708ad9ee8a6a27b8fe
jaeiljoo <sgsgself@naver.com>
2021-10-26 17:05:29 +0900

    [add]카카오로그인 프론트단 작업 완료

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,6 +1,7 @@
 import { createAction, handleActions } from "redux-actions"
 import { produce } from "immer"
 import { api } from "../../shared/api"
+import axios from "axios"
 
 // 액션타입
 
@@ -21,12 +22,38 @@ const initialState = {
 
 // 미들웨어
 
-const log_in_md = ({ user_info }) => {
+const log_in_md = (user_info) => {
   return function (dispatch, getState, { history }) {
+    const { userid, password } = user_info
     api
-      .post("", {})
+      .post("/user", { userid, password })
       .then((res) => console.log(res.data))
       .catch((err) => console.log(err, "로그인에러입니다."))
+  }
+}
+
+const sign_up_md = (user_info) => {
+  return function (dispatch, getState, { history }) {
+    const { userid, username, password } = user_info
+    console.log(userid, username, password)
+    api
+      .post("/signup", { userid, username, password })
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err, "회원가입 에러"))
+  }
+}
+
+// 카카오 로그인
+const kakaoLogin = (key) => {
+  return function (dispatch, getState, { history }) {
+    axios
+      .get(`http://localhost:3000/user/kakao/callback?code=${key}`)
+      .then((res) => {
+        console.log(res)
+
+        // const ACCESS_TOKEN = res.data.accessToken
+      })
+      .catch((err) => console.log(err, "카카오 로그인 실패"))
   }
 }
 
@@ -44,6 +71,8 @@ const actionCreators = {
   log_in,
   log_in_md,
   sign_up,
+  sign_up_md,
+  kakaoLogin,
 }
 
 export { actionCreators }
