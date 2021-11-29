cb85e90f4784c3057dce3c12d9ed206da12eaa7c
나는 신유빈이다 <85838870+sinyubin@users.noreply.github.com>
2021-11-29 23:44:18 +0900

    [커뮤니티]

--- src/communityList/CommunityComment.js
+++ src/communityList/CommunityComment.js
@@ -3,9 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { Text } from "../components";
 import { Comments } from "../componentsGoods/Comments";
-import { actionCreators as actionCr } from "../redux/modules/communityDetail";
+import { actionCreators as detailCr } from "../redux/modules/communityDetail";
 import send from "../shared/icon/send.svg";
-import { actionCreators as communCr } from "../redux/modules/communityDetail";
 const CommunityComment = (props) => {
   // const { communCommentId, commentId } = props;
   const dispatch = useDispatch();
@@ -16,10 +15,22 @@ const CommunityComment = (props) => {
 
   const id = props.communCommentId;
   const commentList = props?.communityCommentList;
+
+  //수정버튼
+  const updateCommentDispatch = (commentId, comment) => {
+    dispatch(detailCr.updateCommunCommentAPI(id, commentId, comment));
+    // console.log(commentId, comment, "실행됐냐도");
+  };
+
+  //삭제버튼
+  const deleteCommentBtn = (commentId) => {
+    dispatch(detailCr.deleteCommunCommrntAPI(id, commentId));
+  };
+
   //추가멘트
   const addComment = () => {
     if (message !== "") {
-      dispatch(actionCr.postCommunCommentAPI(id, message));
+      dispatch(detailCr.postCommunCommentAPI(id, message));
       setMessage("");
       return;
     } else {
@@ -27,6 +38,8 @@ const CommunityComment = (props) => {
       return;
     }
   };
+
+  //업데이트 버튼
 
   return (
     <div>
@@ -36,6 +49,8 @@ const CommunityComment = (props) => {
         commentList.map((e, i) => {
           return (
             <Comments
+              deleteCommentBtn={deleteCommentBtn}
+              updateCommentDispatch={updateCommentDispatch}
               key={e.communityCommentId}
               useridx={user_info.useridx}
               {...e}
