f648de676cab7aa13c2dabcccb00284da81d1836
lipton-web <cpt.lipton@gmail.com>
2021-12-23 14:09:51 +0900

    [추가]useCheckProfile 커스텀훅

--- src/components/customHook/useProfile.js
+++ src/components/customHook/useProfile.js
@@ -4,8 +4,6 @@ export const useProfile = (usertype, imgPath) => {
   // 로그인 타입(카카오, 일반)에 따라 유저 프로필사진 경로설정
 
   const userImg = () => {
-
-    // 로그인 했을 때 프로필사진 체크
     if (usertype === "normal") {
       return process.env.REACT_APP_S3_USER_PROFILE_URL + imgPath
     }
